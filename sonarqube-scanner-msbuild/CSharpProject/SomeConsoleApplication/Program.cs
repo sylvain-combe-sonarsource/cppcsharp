430829e849be9cbc47351473b242acbba86225ac
Services Team <services@sonarsource.com>
2023-07-12 10:16:36 +0200

    initial

---
+++ sonarqube-scanner-msbuild/CSharpProject/SomeConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace SomeConsoleApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            Console.WriteLine(random.Next());
+            
+            var iAmTrue = AlwaysReturnsTrue();
+            if (iAmTrue)
+            {
+                Console.WriteLine("true");
+            }
+
+            Console.ReadKey();
+        }
+
+        private const  bool _alwaysTrue = true;
+        public static bool AlwaysReturnsTrue()
+        {
+            return _alwaysTrue;
+        }
+
+
+        public static object Passthrough(object obj)
+        {
+            return obj;
+        }
+    }
+}
---
+++ sonarqube-scanner-msbuild/Outputs/Prints/SomeConsolePrints/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace SomeConsolePrints
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var iAmTrue = AlwaysReturnsTrue(); //true;
+            if (iAmTrue)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+            Console.ReadKey();
+        }
+
+        // orig for Sonar rule check
+        public static bool AlwaysReturnsTrue()
+        {
+            return true;
+        }
+
+        //private const  bool _alwaysTrue = true;
+        //public static bool AlwaysReturnsTrue()
+        //{
+        //    return _alwaysTrue;
+        //}
+
+
+        public static object Passthrough(object obj)
+        {
+            return obj;
+        }
+    }
+}
